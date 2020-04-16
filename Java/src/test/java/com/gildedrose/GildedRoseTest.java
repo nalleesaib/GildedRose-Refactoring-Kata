5e63551cc1aced77a42920d1f7ed26ad200190bf
Nadeem ALLEESAIB <n.alleesaib@accenture.com>
2020-04-16 13:46:21 +0400

    TDD code legacy

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,6 +6,11 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
+    private static final String SULFURAS="Sulfuras, Hand of Ragnaros";
+    private static final String EXLIXIR="Elixir of the Mongoose";
+    private static final String AGED_BRIE="Aged Brie";
+    private static final String BACKSTAGE="Backstage passes to a TAFKAL80ETC concert";
+
     private Item[] items = new Item[] {
             new Item("+5 Dexterity Vest", 10, 20), //
             new Item("Aged Brie", 2, 0), //
@@ -28,7 +33,87 @@ public class GildedRoseTest {
         app.updateQuality();
 
         // Then
-        Assertions.assertThat(app.items[0].name).isEqualTo("fixme");
+        Assertions.assertThat(app.items[0].name).isEqualTo("foo");
     }
 
+    /*
+        Test using name Sulfuras -> should return
+    */
+    @Test
+    public void testSulfurasQuality0() {
+        // Given
+        Item[] items = new Item[] { new Item(SULFURAS, 0, 0) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        Assertions.assertThat(app.items[0].quality).isEqualTo(0);
+    }
+
+    @Test
+    public void testElixirQuality15() {
+        // Given
+        Item[] items = new Item[] { new Item(EXLIXIR, 0, 15) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        Assertions.assertThat(app.items[0].quality).isEqualTo(13);
+    }
+    @Test
+    public void testAgedBrieQuality15() {
+        // Given
+        Item[] items = new Item[] { new Item(AGED_BRIE, 0, 15) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        Assertions.assertThat(app.items[0].quality).isEqualTo(17);
+    }
+
+    @Test
+    public void testBackstageSelling11Quality15() {
+        // Given
+        Item[] items = new Item[] { new Item(BACKSTAGE, 11, 15) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        Assertions.assertThat(app.items[0].quality).isEqualTo(16);
+    }
+
+    @Test
+    public void testBackstageSelling5Quality15() {
+        // Given
+        Item[] items = new Item[] { new Item(BACKSTAGE, 5, 15) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        Assertions.assertThat(app.items[0].quality).isEqualTo(18);
+    }
+
+    @Test
+    public void testElixirSellingLessThan1Quality15() {
+        // Given
+        Item[] items = new Item[]{new Item(EXLIXIR, -1, 15)};
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        Assertions.assertThat(app.items[0].quality).isEqualTo(13);
+        Assertions.assertThat(app.items[0].sellIn).isEqualTo(-2);
+    }
 }
